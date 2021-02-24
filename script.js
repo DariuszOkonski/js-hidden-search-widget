358b9b12a3c0a6653d3b9af72a51d6c604b44eb4
Dariusz Okonski <darek200180@gmail.com>
2021-02-24 22:03:55 +0000

    add javascript to make transition

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const btn = document.querySelector('[data-button]');
+const widget = document.querySelector('[data-widget]');
+const input = document.querySelector('[data-input]');
+
+btn.addEventListener('click', () => {
+    widget.classList.toggle('active')
+
+    if(widget.classList.contains('active')) {
+        input.focus()
+    }
+}) 
